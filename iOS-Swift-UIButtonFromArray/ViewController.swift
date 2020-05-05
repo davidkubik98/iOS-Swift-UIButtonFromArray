0c6f5d38403675d29e955408f18363043d0a9fb7
Pooya Hatami <am.hatami@gmail.com>
2020-05-05 10:42:28 -0700

    Welcome to iOS UIButtons From Array Tutorial ! IOS , Swift , Tutorial - Make Group of Buttons From Array  (UIButton)

--- iOS-Swift-UIButtonFromArray/ViewController.swift
+++ iOS-Swift-UIButtonFromArray/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonsStack: UIStackView!
+    var myButtonsArray : [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for index in 0...5 {
+            myButtonsArray.append("Button \(index)")
+        }
+        
+        for (index,elemt) in myButtonsArray.enumerated() {
+            let oneBtn : UIButton = {
+                let button = UIButton()
+                button.setTitle(elemt, for: .normal)
+                button.backgroundColor = UIColor.gray
+                button.layer.borderColor = UIColor.yellow.cgColor
+                button.setTitleColor(UIColor.yellow, for: .normal)
+                //button.translatesAutoresizingMaskIntoConstraints = false
+                button.contentHorizontalAlignment = .center
+                button.titleLabel?.font =  UIFont(name: "Arial", size: 20)
+                button.layer.cornerRadius = 5
+                button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+                button.tag = index
+                return button
+            }()
+            buttonsStack.addArrangedSubview(oneBtn)
+
+        }
+    } //End of ViewDidLoad
+
+    @IBAction func buttonAction(sender: UIButton!) {
+        print("Button tapped by tag \(sender.tag)")
     }
-
-
+    
 }
 
