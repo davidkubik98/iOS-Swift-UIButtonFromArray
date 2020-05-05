1dd0be01d498381bc48720556ab7b1435e450255
Pooya Hatami <am.hatami@gmail.com>
2020-05-05 12:14:19 -0700

    Ready

--- iOS-Swift-UIButtonFromArray/ViewController.swift
+++ iOS-Swift-UIButtonFromArray/ViewController.swift
@@ -16,32 +16,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for index in 0...5 {
+        for index in 0...7 {
             myButtonsArray.append("Button \(index)")
         }
         
-        for (index,elemt) in myButtonsArray.enumerated() {
+        for (index,element) in myButtonsArray.enumerated() {
             let oneBtn : UIButton = {
                 let button = UIButton()
-                button.setTitle(elemt, for: .normal)
+                button.setTitle(element, for: .normal)
                 button.backgroundColor = UIColor.gray
                 button.layer.borderColor = UIColor.yellow.cgColor
+                button.layer.borderWidth = 1
                 button.setTitleColor(UIColor.yellow, for: .normal)
                 //button.translatesAutoresizingMaskIntoConstraints = false
                 button.contentHorizontalAlignment = .center
-                button.titleLabel?.font =  UIFont(name: "Arial", size: 20)
+                button.contentVerticalAlignment = .center
+                button.titleLabel?.font = UIFont(name: "Arial", size: 22)
                 button.layer.cornerRadius = 5
                 button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
                 button.tag = index
                 return button
             }()
             buttonsStack.addArrangedSubview(oneBtn)
-
+            
         }
-    } //End of ViewDidLoad
-
+        
+        
+    } //End ViewDidload
+    
     @IBAction func buttonAction(sender: UIButton!) {
-        print("Button tapped by tag \(sender.tag)")
+        print("Button tapped with tag \(sender.tag)")
     }
     
 }
